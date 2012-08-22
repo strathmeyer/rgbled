da41768672a3c1425a30729ea3d28059215679f4
Eric Strathmeyer <strathmeyer@squareup.com>
2012-08-22 01:32:19 -0700

    Clean up

--- rgbled.ino
+++ rgbled.ino
@@ -1,20 +1,18 @@
 /*
  Fade
- 
+
  A program to fade between colors with a RBG LED 
- 
- */
+*/
+
 #include <math.h>
- 
+
+// Input potentiometer
+#define speedInput A5
+
+// Output pins
 #define rPin 11
 #define gPin 10
 #define bPin 6
- 
-#define speedInput A5
-// 
-//int r = 0;
-//int g = 120;
-//int b = 240;
 
 byte r;
 byte g;
@@ -27,26 +25,32 @@ void setup()  {
   pinMode(gPin, OUTPUT);
   pinMode(bPin, OUTPUT);
   pinMode(A0, INPUT);
-  
+
   analogWrite(rPin, 0);
   analogWrite(gPin, 0);
   analogWrite(bPin, 0);
-  
-//  Serial.begin(9600);
+
+  // DEBUG:
+  // Serial.begin(9600);
 }
 
 double degToRad(int degrees) {
   return 0.0174532925 * degrees;
 }
 
-double fmap(double x, double in_min, double in_max, double out_min, double out_max) {
-  return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+double fmap(double x,
+            double in_min, double in_max,
+            double out_min, double out_max) {
+
+  return  (x - in_min) * (out_max - out_min) /
+          (in_max - in_min) + out_min;
 }
 void updatePosition(double &degrees) {
   unsigned int potVal = analogRead(speedInput);
-  double speed = fmap(pow(potVal/1024.0, 5.0), 0.0, pow(1024.0/1024.0, 5.0), 0.01, 100.0);
+  double speed = fmap(pow(potVal/1024.0, 5.0),
+                      0.0, 1.0),
+                      0.001, 100.0);
   degrees = fmod((degrees + speed), 360.0);
-//  Serial.println(speed);
 }
 
 double sineRange(double input, double rangeMin, double rangeMax) {
@@ -58,25 +62,24 @@ int ledValue(int position, double scale=1.0) {
    return sineRange(degToRad(position), 10, 255 * scale);
 }
 
-void HSV_to_RGB(double h, double s, double v, byte &r, byte &g, byte &b)
-{
+void HSV_to_RGB(double h, double s, double v, byte &r, byte &g, byte &b) {
   int i;
   double f,p,q,t;
-  
+
   h = constrain(h, 0.0, 360.0);
   s = constrain(s, 0.0, 100.0);
   v = constrain(v, 0.0, 100.0);
-  
+
   s /= 100;
   v /= 100;
-  
+
   if (s == 0) {
-    // Achromatic (grey)
+    // grey
     r = g = b = round(v*255);
     return;
   }
 
-  h /= 60.0; 
+  h /= 60.0;
   i = floor(h); // sector 0 to 5
   f = h - (double)i; // factorial part of h
   p = v * (1.0 - s);
@@ -116,20 +119,21 @@ void HSV_to_RGB(double h, double s, double v, byte &r, byte &g, byte &b)
 }
 
 // the loop routine runs over and over again forever:
-void loop()  {   
+void loop()  {
   updatePosition(h);
-  
+
   HSV_to_RGB((double)h, 100.0, 100.0, r, g, b);
-  
+
   analogWrite(rPin, r);
   analogWrite(gPin, g);
   analogWrite(bPin, b);
 
-//  Serial.print(r);
-//  Serial.print(", ");
-//  Serial.print(g);
-//  Serial.print(", ");
-//  Serial.println(b);
+  // DEBUG:
+  // Serial.print(r);
+  // Serial.print(", ");
+  // Serial.print(g);
+  // Serial.print(", ");
+  // Serial.println(b);
 
   delay(30);
 }
